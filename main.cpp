f432029e4e058e324c0bef70b8e6fa127690f732
SokolowskiKrzysztof <krzych1257@interia.pl>
2017-09-25 16:53:29 +0200

    zadanie1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
-
+string imie = "dagmara";
+string nazwisko = "weremiuk";
+int rok_urodzenia = 1997;
+int aktualny_rok = 2017;
+int lat=aktualny_rok-rok_urodzenia;
 int main()
 {
-    cout << "Hello world!" << endl;
+
+    cout <<"Witam, nazywam sie "<<imie <<" "<<nazwisko<<" "<<"i mam "<<lat<<endl;
     return 0;
 }
